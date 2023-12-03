3a06f111be63580af14a46144b714b18ad34d067
CosimoA <cosimo.archip@gmail.com>
2023-12-03 14:44:37 +0100

    CONFIG: setup base scaffolding & js flux

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/*  Descrizione:
+Visualizzare in pagina 5 numeri casuali.
+Da lì parte un timer di 30 secondi. Dopo 30 secondi i numeri scompaiono e l’utente deve inserire, i numeri che ha visto precedentemente.
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. 
+*/
+
+// VARIABILI PREIMPOSTATE
+
+// GENERA 5 NUMERI RANDOM DA PUSHARE IN UN ARRAY
+
+// MOSTRA I NUMRI GENERATI SULLO SCHERMO
+
+// DOPO 30 SECONDI I NUMERI SPARISCONO e parte il gioco
+    // i 5 numeri vengono nascosti
+    // creo array vuoto dove l'user inserisce i numeri che ricorda
+    // chiedo all'utente per 5 volte di inserire un numero
+    // VERIFICO SE I NUMERI COMBACIANO CON I 5 RANDOM
+        // SE COMBACIANO LOG HAI INDOVINATO NUMERO e PUSH NELL'ARRAY INDOVINATI
+        // SE NON COMBACIANO PUSH NELL'ARRAY DEI NUMERI SBAGLIATI
+
+    // DICHIARO ALL'UTENTE SE HA VINTO O A PERSO e gli mostro i numeri indovinati o quelli sbagliati
+
+
+
+
+
+// ---------------------FUNZIONI---------------------
+
