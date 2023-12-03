3e709c7fc29e4666906e4b02193dcd5c549ed029
CosimoA <cosimo.archip@gmail.com>
2023-12-03 15:23:09 +0100

    FEAT: generate & append 5 random numbers

--- js/script.js
+++ js/script.js
@@ -5,10 +5,21 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 */
 
 // VARIABILI PREIMPOSTATE
+    // ARRAY'S
+const startRandomNumbers = [];
+const numeriIndovinati = [];
+const numeriSbagliati = [];
+    // HTML ELEMENTS
+const startContainer = document.getElementById("container");
+const finishContainer = document.getElementById("numbers");
 
 // GENERA 5 NUMERI RANDOM DA PUSHARE IN UN ARRAY
+creaArrRandomNum(1, 100, 5);
+startRandomNumbers.push(creaArrRandomNum(1, 100, 5));
+console.log("I 5 numeri iniziali sono: " + startRandomNumbers);
 
 // MOSTRA I NUMRI GENERATI SULLO SCHERMO
+startContainer.append(startRandomNumbers);
 
 // DOPO 30 SECONDI I NUMERI SPARISCONO e parte il gioco
     // i 5 numeri vengono nascosti
@@ -26,3 +37,21 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 
 // ---------------------FUNZIONI---------------------
 
+
+// FUNZIONE CHE GENERA NUMERO RANDOM IN UN RANGE
+function creaNumRandomMinMax(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// FUNZIONE CHE CREA UN ARRAY CON NUMERI IN ORDINE CASUALE IN UN RANGE
+function creaArrRandomNum(minNum, maxNumm, lunghezza){
+    const arrayRandom = [];
+    while (arrayRandom.length < lunghezza){
+        let numRandom = creaNumRandomMinMax(minNum, maxNumm);
+        // SE IL NUMERO NON è PRESENTE NELL'ARRAY LO PUSHO
+        if (!arrayRandom.includes(numRandom)){
+            arrayRandom.push(numRandom);
+        }
+    }
+    return arrayRandom;
+}
